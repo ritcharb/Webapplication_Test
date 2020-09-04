65ed636951a6039c665676540f20ccc79bd7d343
RICHYA ACHARYA <65505364+ritcharb@users.noreply.github.com>
2020-09-04 13:42:34 +0200

    Add files via upload
    Issue of xml file updated

---
+++ TestNGMain.java
@@ -0,0 +1,20 @@
+
+package test;
+import java.util.List;
+
+import org.testng.TestListenerAdapter;
+import org.testng.TestNG;
+import org.testng.collections.Lists;
+//testxml file invokes and executed using testNG.run()
+
+public class TestNGMain {
+	public static void main(String[] args) {
+	    TestListenerAdapter tla = new TestListenerAdapter();
+	    TestNG testng = new TestNG();
+	    List<String> suites = Lists.newArrayList();
+	    suites.add("testng.xml");//path to xml..
+	    testng.setTestSuites(suites);
+	    testng.run();
+	}
+
+}
